9d54c5790d016ccb0a3c8a876295a1bc0b9730a7
hambergss <144480774+hambergss@users.noreply.github.com>
2025-01-28 14:04:15 -0500

    s

--- app/src/views/AboutView.vue
+++ app/src/views/AboutView.vue
@@ -1,11 +1,25 @@
 <template>
   <div>
-    
+    <form @submit.prevent="login(user)" action="submit">
+      <label for="username">Username</label>
+      <input type="text" v-model="user.username" />
+      <label for="password"></label>
+      <input type="password" v-model="user.password" />
+      <button>Submit</button>
+    </form>
+    <h2 v-if="loggedIn">Hello Peter</h2>
+    <h2 v-else>Please Login</h2>
   </div>
 </template>
 
 <script setup>
 import { ref, reactive } from 'vue'
+const user = reactive({ username: '', password: '' })
+function login(user) {
+  console.log(user)
+}
+
+const loggedIn = false
 </script>
 
 <style scoped></style>
--- app/vite.config.js
+++ app/vite.config.js
@@ -1,14 +1,18 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import daisyui from 'daisyui'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    tailwindcss(),
+    require('daisyui'),
   ],
   resolve: {
     alias: {
