bf41debbb92bb395f43e223a784d26ad2f8eca06
hambergss <144480774+hambergss@users.noreply.github.com>
2025-02-04 14:00:24 -0500

    Update HomeView.vue

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,12 +1,69 @@
 <template>
   <div>
-    <h2 class="mx-auto justify-self-center ">Genshin Popup Event</h2>
-
+    <h2 class="mx-auto justify-self-center">Genshin Popup Event</h2>
   </div>
 </template>
 
 <script setup>
 import { ref, reactive } from 'vue'
+const itemsArray = [
+  {
+    category: 'Keychains',
+    items: [
+      'Paimon Keychain',
+      'Klee Keychain',
+      'Diluc Keychain',
+      'Zhongli Keychain',
+      'Venti Keychain',
+      'Raiden Shogun Keychain',
+      'Xiao Keychain',
+      'Barbara Keychain',
+    ],
+  },
+  {
+    category: 'Plushies',
+    items: [
+      'Paimon Plushie', 
+      'Klee Plushie', 
+      'Qiqi Plushie', 
+      'Amber Plushie', 
+      'Barbara Plushie', 
+    ],
+  },
+  {
+    category: 'Pins',
+    items: [
+      'Paimon Pin', 
+      'Klee Pin', 
+      'Diluc Pin', 
+      'Zhongli Pin', 
+      'Venti Pin', 
+      'Raiden Shogun Pin', 
+    ],
+  },
+  {
+    category: 'T-shirts',
+    items: [
+      'Klee T-shirt',
+      'Diluc T-shirt',
+      'Zhongli T-shirt',
+      'Venti T-shirt',
+      'Raiden Shogun T-shirt',
+    ],
+  },
+  {
+    category: 'Phone Cases',
+    items: [
+      'Paimon Phone Case',
+      'Klee Phone Case',
+      'Diluc Phone Case',
+      'Zhongli Phone Case',
+      'Venti Phone Case',
+    ],
+  },
+]
+
+
 </script>
 
 <style scoped></style>
