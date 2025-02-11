ffe80626f05aa899c95104cd89c551a050367381
hambergss <144480774+hambergss@users.noreply.github.com>
2025-02-11 14:01:31 -0500

    s

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,16 +1,199 @@
 <template>
-  <div>
-    <FilterButtons v-for="switche in switches" :key="switche.name" />
+  <div id="app">
+    <header>
+      <h1>Keyboard Switch Store</h1>
+      <button @click="addSwitch">Add New Switch</button>
+    </header>
+    <section>
+      <FilterButtons v-for="switche in switches" :key="switche.name" :switche="switche" />
+    </section>
+    <footer>
+      <p>Switches</p>
+    </footer>
   </div>
 </template>
 
 <script setup>
+import { name } from '@vue/eslint-config-prettier/skip-formatting';
 import FilterButtons from './components/FilterButtons.vue'
-switches.forEach((switche) =>
-  document
-    .querySelector('.container')
-    .insertAdjacentElement(`<div className="card"><h1>${switche.name}</h1></div>`),
-)
+const switches = reactive([
+  {
+    manufacturer: 'Gateron',
+    name: 'Gateron Red',
+    type: 'linear',
+    sound: 'creamy',
+  },
+  {
+    manufacturer: 'Gateron',
+    name: 'Gateron Milky Yellow Pro (KS-3)',
+    type: 'linear',
+    sound: 'creamy',
+  },
+  {
+    manufacturer: 'HMX',
+    name: 'HMX Poro',
+    type: 'linear',
+    sound: 'thocky',
+  },
+  {
+    manufacturer: 'KTT',
+    name: ' KTT Strawberry',
+    type: 'linear',
+    sound: 'creamy',
+  },
+  {
+    manufacturer: 'Wuque Studios',
+    name: 'WS Morandi',
+    type: 'linear',
+    sound: 'creamy',
+  },
+  {
+    manufacturer: 'Akko',
+    name: 'V3 Cream Yellow Pro',
+    type: 'linear',
+    sound: 'creamy',
+  },
+  {
+    manufacturer: 'Gateron',
+    name: 'Brown',
+    type: 'tactile',
+    sound: 'thocky',
+  },
+  {
+    manufacturer: 'Akko',
+    name: 'Penguin',
+    type: 'tactile',
+    sound: 'thocky',
+  },
+  {
+    manufacturer: 'KTT',
+    name: 'Lemon',
+    type: 'tactile',
+    sound: 'thocky',
+  },
+  {
+    manufacturer: 'Invyr',
+    name: 'Holy Panda',
+    type: 'tactile',
+    sound: 'thocky',
+  },
+  {
+    manufacturer: 'Akko',
+    name: 'V3 Creamy Purple Pro',
+    type: 'tactile',
+    sound: 'thocky',
+  },
+
+  {
+    manufacturer: 'Gateron',
+    name: 'Blue',
+    type: 'clicky',
+    sound: 'clicky',
+  },
+  {
+    manufacturer: 'Gateron',
+    name: 'Melodic',
+    type: 'clicky',
+    sound: 'clicky',
+  },
+  {
+    manufacturer: 'Akko',
+    name: 'Creamy Cyan',
+    type: 'clicky',
+    sound: 'clicky',
+  },
+  {
+    manufacturer: 'Wuque Studios',
+    name: 'Clicky Switch',
+    type: 'clicky',
+    sound: 'clicky',
+  },
+  {
+    manufacturer: 'Gateron',
+    name: 'Silent Red',
+    type: 'linear',
+    sound: 'silent',
+  },
+  {
+    manufacturer: 'Gateron',
+    name: 'Silent Brown',
+    type: 'tactile',
+    sound: 'silent',
+  },
+  {
+    manufacturer: 'Akko',
+    name: 'Silent Red',
+    type: 'linear',
+    sound: 'silent',
+  },
+  {
+    manufacturer: 'Akko',
+    name: 'Silent Brown',
+    type: 'tactile',
+    sound: 'silent',
+  },
+  {
+    manufacturer: 'HMX',
+    name: 'HMX Cheese',
+    type: 'linear',
+    sound: 'clacky',
+  },
+  {
+    manufacturer: 'HMX',
+    name: 'HMX Xinhai',
+    type: 'linear',
+    sound: 'clacky',
+  },
+  {
+    manufacturer: 'Gateron',
+    name: 'Gateron Quinn',
+    type: 'tactile',
+    sound: 'clacky',
+  },
+  {
+    manufacturer: 'Gateron',
+    name: 'Gateron Baby Kangaroo (V2)',
+    type: 'tactile',
+    sound: 'clacky',
+  },
+  {
+    manufacturer: 'HMX',
+    name: 'HMX Swift',
+    type: 'linear',
+    sound: 'clacky',
+  },
+  {
+    manufacturer: 'HMX',
+    name: 'HMX Deep Navy',
+    type: 'linear',
+    sound: 'clacky',
+  },
+  {
+    manufacturer: 'HMX',
+    name: 'HMX Macchiato',
+    type: 'linear',
+    sound: 'clacky',
+  },
+  {
+    manufacturer: 'HMX',
+    name: 'HMX Sunset Gleam',
+    type: 'linear',
+    sound: 'clacky',
+  },
+])
+function addSwitch() {
+  switches.value.push({
+    name: `Switch ${Date.now()}`,
+    manufacturer: 'Unknown',
+    type: 'linear',
+    sound: 'creamy',
+  })
+}
 </script>
 
-<style scoped></style>
+<style scoped>
+#app {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
--- app/src/components/FilterButtons.vue
+++ app/src/components/FilterButtons.vue
@@ -15,176 +15,16 @@
         {{ switche.name }} - {{ switche.manufacturer }} - {{ switche.type }} - {{ switche.sound }}
       </li>
     </ul>
+    <div class="card">
+      <h1>{{ switche.name }}</h1>
+      <p>{{ switche.manufacturer }} - {{ switche.type }} - {{ switche.sound }}</p>
+    </div>
   </div>
 </template>
 
 <script setup>
 import { ref, reactive } from 'vue'
-const switches = reactive([
-  {
-    manufacturer: 'Gateron',
-    name: 'Gateron Red',
-    type: 'linear',
-    sound: 'creamy',
-  },
-  {
-    manufacturer: 'Gateron',
-    name: 'Gateron Milky Yellow Pro (KS-3)',
-    type: 'linear',
-    sound: 'creamy',
-  },
-  {
-    manufacturer: 'HMX',
-    name: 'HMX Poro',
-    type: 'linear',
-    sound: 'thocky',
-  },
-  {
-    manufacturer: 'KTT',
-    name: ' KTT Strawberry',
-    type: 'linear',
-    sound: 'creamy',
-  },
-  {
-    manufacturer: 'Wuque Studios',
-    name: 'WS Morandi',
-    type: 'linear',
-    sound: 'creamy',
-  },
-  {
-    manufacturer: 'Akko',
-    name: 'V3 Cream Yellow Pro',
-    type: 'linear',
-    sound: 'creamy',
-  },
-  {
-    manufacturer: 'Gateron',
-    name: 'Brown',
-    type: 'tactile',
-    sound: 'thocky',
-  },
-  {
-    manufacturer: 'Akko',
-    name: 'Penguin',
-    type: 'tactile',
-    sound: 'thocky',
-  },
-  {
-    manufacturer: 'KTT',
-    name: 'Lemon',
-    type: 'tactile',
-    sound: 'thocky',
-  },
-  {
-    manufacturer: 'Invyr',
-    name: 'Holy Panda',
-    type: 'tactile',
-    sound: 'thocky',
-  },
-  {
-    manufacturer: 'Akko',
-    name: 'V3 Creamy Purple Pro',
-    type: 'tactile',
-    sound: 'thocky',
-  },
 
-  {
-    manufacturer: 'Gateron',
-    name: 'Blue',
-    type: 'clicky',
-    sound: 'clicky',
-  },
-  {
-    manufacturer: 'Gateron',
-    name: 'Melodic',
-    type: 'clicky',
-    sound: 'clicky',
-  },
-  {
-    manufacturer: 'Akko',
-    name: 'Creamy Cyan',
-    type: 'clicky',
-    sound: 'clicky',
-  },
-  {
-    manufacturer: 'Wuque Studios',
-    name: 'Clicky Switch',
-    type: 'clicky',
-    sound: 'clicky',
-  },
-  {
-    manufacturer: 'Gateron',
-    name: 'Silent Red',
-    type: 'linear',
-    sound: 'silent',
-  },
-  {
-    manufacturer: 'Gateron',
-    name: 'Silent Brown',
-    type: 'tactile',
-    sound: 'silent',
-  },
-  {
-    manufacturer: 'Akko',
-    name: 'Silent Red',
-    type: 'linear',
-    sound: 'silent',
-  },
-  {
-    manufacturer: 'Akko',
-    name: 'Silent Brown',
-    type: 'tactile',
-    sound: 'silent',
-  },
-  {
-    manufacturer: 'HMX',
-    name: 'HMX Cheese',
-    type: 'linear',
-    sound: 'clacky',
-  },
-  {
-    manufacturer: 'HMX',
-    name: 'HMX Xinhai',
-    type: 'linear',
-    sound: 'clacky',
-  },
-  {
-    manufacturer: 'Gateron',
-    name: 'Gateron Quinn',
-    type: 'tactile',
-    sound: 'clacky',
-  },
-  {
-    manufacturer: 'Gateron',
-    name: 'Gateron Baby Kangaroo (V2)',
-    type: 'tactile',
-    sound: 'clacky',
-  },
-  {
-    manufacturer: 'HMX',
-    name: 'HMX Swift',
-    type: 'linear',
-    sound: 'clacky',
-  },
-  {
-    manufacturer: 'HMX',
-    name: 'HMX Deep Navy',
-    type: 'linear',
-    sound: 'clacky',
-  },
-  {
-    manufacturer: 'HMX',
-    name: 'HMX Macchiato',
-    type: 'linear',
-    sound: 'clacky',
-  },
-  {
-    manufacturer: 'HMX',
-    name: 'HMX Sunset Gleam',
-    type: 'linear',
-    sound: 'clacky',
-  },
-])
 const filteredSwitches = ref(switches)
 
 function filterSwitches(manufacturer) {
@@ -195,7 +35,15 @@ function filterByType(type) {
   filteredSwitches.value = switches.filter((switche) => switche.type === type)
 }
 
-
+defineProps({
+  switche: Object,
+})
 </script>
 
-<style scoped></style>
+<style scoped>
+.card {
+  padding: 10px;
+  border: 1px solid #ccc;
+  margin: 10px;
+}
+</style>
